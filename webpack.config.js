ad8d876075b04daf68d41a9e9156a8c26fcdbcae
Amarnath <sharewithamar@gmail.com>
2021-02-24 20:38:37 +0530

    adding babel config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devtool: false,
+  devServer: {
+    contentBase: './dist',
+  },
+};
