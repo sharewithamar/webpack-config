f1b1aa5b14030b949e4748a24ad7038909b646d2
Amarnath <sharewithamar@gmail.com>
2021-02-25 12:07:06 +0530

    universal test for sass,scss,css

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s?css$/i,
+        //test: /\.s?css$/i,
+        test: /\.(s[ac]|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
