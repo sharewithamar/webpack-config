ba1654ec65dba6498a429aee028dc59e46ebd847
Amarnath <sharewithamar@gmail.com>
2021-02-25 12:02:22 +0530

    adding borwserlist prod check

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
+let target = 'web';
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+  target = 'browserslist';
 }
 module.exports = {
   mode: mode,
+  target: target,
+
   module: {
     rules: [
       {
