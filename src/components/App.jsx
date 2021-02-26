e0c9cc4333ae85ab8a23497e030d1fe95ad48a30
Amarnath <sharewithamar@gmail.com>
2021-02-26 13:38:24 +0530

    adding react refresh for fast development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import '../styles/index.scss';
 import Recipes from './Recipes';
-import sword from '../images/swc-sword.png';
-import swordSvg from '../images/sword.svg';
+import Whatever from './Whatever';
+// import sword from '../images/swc-sword.png';
+// import swordSvg from '../images/sword.svg';
+
 const App = (props) => {
   return (
     <>
       <section className="hero"></section>
       <main>
         <section>
-          <h1>Oh Hai, React</h1>
+          <h1>Oh hioooo Hello Amar, React</h1>
         </section>
-        <img src={sword} alt="sword" width="250" />
-        <img src={swordSvg} alt="sword" width="250" />
+        {/* <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" /> */}
 
         <Recipes />
+        <Whatever />
       </main>
     </>
   );
