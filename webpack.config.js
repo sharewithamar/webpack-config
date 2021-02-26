9c74a9602ebd20bc6066642ed514bb9101a6ea90
Amarnath <sharewithamar@gmail.com>
2021-02-26 11:09:17 +0530

    clean-webpack-plugin and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 let mode = 'development';
 let target = 'web';
 if (process.env.NODE_ENV === 'production') {
@@ -11,6 +13,7 @@ module.exports = {
   target: target,
 
   output: {
+    path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'images/[hash][ext][query]',
   },
   module: {
@@ -46,7 +49,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 
   resolve: {
     extensions: ['.js', '.jsx'],
