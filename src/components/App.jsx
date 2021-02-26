c5b2db43bdd06e33f60114feb5626e8066c80ed9
Amarnath <sharewithamar@gmail.com>
2021-02-26 10:03:26 +0530

    image support using asset module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import '../styles/index.scss';
 import Recipes from './Recipes';
+import sword from '../images/swc-sword.png';
+import swordSvg from '../images/sword.svg';
 const App = (props) => {
   return (
     <>
@@ -8,8 +10,11 @@ const App = (props) => {
         <section>
           <h1>Oh Hai, React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+
+        <Recipes />
       </main>
-      <Recipes />
     </>
   );
 };
